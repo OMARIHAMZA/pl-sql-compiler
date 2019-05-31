c5a8ae7e7f9bd7de430efa8b9cbac486fb8083f6
OMARIHAMZA <36658089+OMARIHAMZA@users.noreply.github.com>
2019-05-31 18:29:15 +0300

    added field_terminator to join loop

--- ruby/GeneratedMain.rb
+++ ruby/GeneratedMain.rb
@@ -22,74 +22,55 @@ ordering_columns = []
 selection_columns = []
 records = []
 
-join_type = "null"
-    products_table_location, products_field_terminator = ExecutionPlanUtilities.get_table_location("products")
-    products_csv_files = ExecutionPlanUtilities.get_csv_files(products_table_location)
-    products_file_index = 0
-    products_pos = 0
 
-    until products_file_index == products_csv_files.length
+  ExecutionPlanUtilities::write_to_execution_plan("Nested Loop Join")
+join_type = "JOIN"
+pos_1_product_id_index=  ExecutionPlanUtilities::get_column_index("pos", "product_id")
+products_0_id_index=  ExecutionPlanUtilities::get_column_index("products", "id")
+pos_1_table_location, pos_field_terminator = ExecutionPlanUtilities.get_table_location("pos")
+pos_1_csv_files = ExecutionPlanUtilities.get_csv_files(pos_1_table_location)
+pos_1_file_index = 0
+pos_1_pos = 0
 
-      products_line, products_file_index, products_pos = ExecutionPlanUtilities.read_record(products_table_location, products_csv_files, products_file_index, products_pos,  products_field_terminator)
+until pos_1_file_index == pos_1_csv_files.length
 
-      records << products_line.chomp if true
+  pos_1_line, pos_1_file_index, pos_1_pos = ExecutionPlanUtilities.read_record(pos_1_table_location, pos_1_csv_files, pos_1_file_index, pos_1_pos, pos_field_terminator)
+  record_1 = "" + pos_1_line.chomp
+
+    pos_1_joined_flag = false
+    products_0_table_location, products_field_terminator = ExecutionPlanUtilities.get_table_location("products")
+    products_0_csv_files = ExecutionPlanUtilities.get_csv_files(products_0_table_location)
+    products_0_file_index = 0
+    products_0_pos = 0
+
+    until products_0_file_index == products_0_csv_files.length
+
+      products_0_line, products_0_file_index, products_0_pos = ExecutionPlanUtilities.read_record(products_0_table_location, products_0_csv_files, products_0_file_index, products_0_pos, products_field_terminator)
+      record_0 = record_1 + "," + products_0_line.chomp
+
+        products_0_joined_flag = false
+            if pos_1_line.split(",")[pos_1_product_id_index].strip.to_i==products_0_line.split(",")[products_0_id_index].strip.to_i
+
+        products_0_joined_flag = true
+
+        records <<  record_0
 
     end
+
+
+    end
+
+
+      records << pos_1_line.chomp + ("," * 2) if !pos_1_joined_flag and join_type.start_with?("LEFT")
+
+      records << (("," * 2) + pos_1_line.chomp) if !pos_1_joined_flag and join_type.start_with?("RIGHT")
+
+end
 
 having_conditions = []
 aggregation_columns = []
 
 
-
-records.sort_by!{|record| [ ]}
-
-unless selection_columns.empty?
-records.map!{|record| record.split(",").values_at(*selection_columns).join(",")}
-end
-
-records.uniq! if false
-  unless aggregation_columns.empty?
-
-  mapper_file_name = MapReduce::Mapper.new.map(records, grouping_columns, aggregation_columns)
-
-  shuffler_file_name = ""
-
-  shuffler_file_name = MapReduce::Shuffler.new(mapper_file_name).shuffle unless grouping_columns.empty?
-
-  MapReduce::Reducer.new(grouping_columns.empty? ? mapper_file_name : shuffler_file_name, grouping_columns, aggregation_columns, having_conditions).reduce
-
-  puts File.read(MapReduce::REDUCER_RESULT_FILE)
-
-  end
-
-puts records if aggregation_columns.empty? && ! true
-
-  ExecutionPlanUtilities::write_query_to_file(records, query_counter)
-query_counter += 1
-join_type = "null"
-    q_table_location, q_field_terminator = ExecutionPlanUtilities.get_table_location("q")
-    q_csv_files = ExecutionPlanUtilities.get_csv_files(q_table_location)
-    q_file_index = 0
-    q_pos = 0
-
-    until q_file_index == q_csv_files.length
-
-      q_line, q_file_index, q_pos = ExecutionPlanUtilities.read_record(q_table_location, q_csv_files, q_file_index, q_pos,  q_field_terminator)
-
-      records << q_line.chomp if true
-
-    end
-
-having_conditions = []
-aggregation_columns = []
-
-
-
-if aggregation_columns.empty?
-
-selection_columns << 0 + ExecutionPlanUtilities::get_column_index("q", "name")
-
-end
 
 records.sort_by!{|record| [ ]}
 
